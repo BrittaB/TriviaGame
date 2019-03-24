cedcf09e0baa29fa92da10f4867a021b80725186
Britta <polished.athena@gmail.com>
2019-03-24 10:43:03 -0500

    psuedo code javascript

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//variables
+
+//start page, have no idea how to do it.  Button is hit goes to questions and starts timer
+
+//timer starts when start button is hit
+
+//player is able to make a selection of the answers 
+
+//when correct answer is selected a point is counted in win total
+
+//when incorrect answer is selected a point is counted in the loose total
+
+//when timer runs out player is unable to make any more selections
